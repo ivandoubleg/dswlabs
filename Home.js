c8c3b77d3d413a526b6766848664f82c732d0468
mautaboo <Mauricio_taboo.97@outlook.com>
2021-02-16 23:51:57 -0600

    prueba de subida

---
+++ Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Footer from './Components/Footer';
+
+
+class Home extends Component {
+    render() { 
+        return (  
+            <div className='allev'>
+                <Footer />
+            </div>
+
+        );
+    }
+}
+ 
+export default Home;
